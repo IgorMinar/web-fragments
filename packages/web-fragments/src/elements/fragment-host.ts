f01a8ad43ccfe79c96dcf192d62586ebd4d7d2cd
Igor Minar <i@igor.dev>
2024-12-05 12:37:42 +0100

    WIP: test suite and various patches to be broken up
    Co-authored-by: Natalia Venditto <anfibiacreativa@users.noreply.github.com>

--- packages/web-fragments/src/elements/fragment-host.ts
+++ packages/web-fragments/src/elements/fragment-host.ts
@@ -21,7 +21,7 @@ export class FragmentHost extends HTMLElement {
 
 			const { iframe, ready } = reframed(this.shadowRoot ?? document.location.href, {
 				container: this,
-				headers: { 'x-fragment-mode': 'embedded' },
+				headers: { 'x-fragment-mode': 'embedded', 'x-fragment-id': fragmentId },
 			});
 
 			this.iframe = iframe;
